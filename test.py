9f5f5f7ca3714c44c848ce481d38bfdbd337eaca
parvathy <parvathykalesh743@gmail.com>
2025-08-13 12:05:26 +0530

    TEST

--- test.py
+++ test.py
@@ -1,7 +1,18 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-df = pd.read_csv('data/phishing_data_fixed.csv')  # Replace with your actual CSV file path
-print(df.columns)
+# Load dataset
+data = pd.read_csv('data/phishing_data_fixed.csv')  # adjust path if needed
+
+sns.countplot(x='URL_Length', hue='Result', data=data)
+plt.show()
+
+
+  # Hrow many missing values per column
+
+     # First few rows
+
 
 
 
